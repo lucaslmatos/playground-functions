e6ad2578bbd6dd34105f0291af4d0b433c5991f6
Lucas Matos <lucaslmatos18@gmail.com>
2022-10-29 18:41:20 -0300

    C18 - Reduzi complexidade de 10 para 07 - Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,20 +1,40 @@
 // Desafio 11 (Crie uma função de número de telefone)
-// Pensando em uma alternativa para diminuir a complexidade cognitiva atual de 13 para o limite, que é 5.
+// Salvando código mais próximo do correto para testes:
+// if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
+//   for (let index = 0; index < phoneNumber.length; index += 1) {
+//     let cont = 0;
+//     for (let index2 = 0; index2 < phoneNumber.length; index2 += 1) {
+//       if (phoneNumber[index] === phoneNumber[index2]) {
+//         cont += 1;
+//       }
+//     } if (phoneNumber[index] < 0 || phoneNumber[index] > 9 || cont > 2) {
+//       return 'não é possível gerar um número de telefone com esses valores';
+//     }
+//   }
+//   let number = phoneNumber.join('');
+//   return `(${number.substring(0, 2)}) ${number.substring(2, 7)}-${number.substring(7, 11)}`;
+// Fim do Código
+// Pensando em uma alternativa para diminuir a complexidade cognitiva atual de 10 para o limite, que é 5.
 // eslint-disable-next-line complexity
 function generatePhoneNumber(phoneNumber) {
   if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
   for (let index = 0; index < phoneNumber.length; index += 1) {
-    let cont = 0;
-    for (let index2 = 0; index2 < phoneNumber.length; index2 += 1) {
-      if (phoneNumber[index] === phoneNumber[index2]) {
-        cont += 1;
-      }
-    } if (phoneNumber[index] < 0 || phoneNumber[index] > 9 || cont > 2) {
-      return 'não é possível gerar um número de telefone com esses valores';
-    }
+    if (phoneNumber[index] < 0
+  || phoneNumber[index] > 9) return 'não é possível gerar um número de telefone com esses valores';
   }
-  let number = phoneNumber.join('');
-  return `(${number.substring(0, 2)}) ${number.substring(2, 7)}-${number.substring(7, 11)}`;
+  let num = phoneNumber.join(''); phoneNumber = phoneNumber.sort(function (a, b) { return b - a; });
+  if ((phoneNumber[0] === Math.sqrt(phoneNumber[1] * phoneNumber[2]))
+    || (phoneNumber[1] === Math.sqrt(phoneNumber[2] * phoneNumber[3]))
+    || (phoneNumber[2] === Math.sqrt(phoneNumber[3] * phoneNumber[4]))
+    || (phoneNumber[3] === Math.sqrt(phoneNumber[4] * phoneNumber[5]))
+    || (phoneNumber[4] === Math.sqrt(phoneNumber[5] * phoneNumber[6]))
+    || (phoneNumber[5] === Math.sqrt(phoneNumber[6] * phoneNumber[7]))
+    || (phoneNumber[6] === Math.sqrt(phoneNumber[7] * phoneNumber[8]))
+    || (phoneNumber[7] === Math.sqrt(phoneNumber[8] * phoneNumber[9]))
+    || (phoneNumber[8] === Math.sqrt(phoneNumber[9] * phoneNumber[10]))) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  return `(${num.substring(0, 2)}) ${num.substring(2, 7)}-${num.substring(7, 11)}`;
 }
 
 // Desafio 12 (Crie uma função que teste a condição de existência de um triângulo)
