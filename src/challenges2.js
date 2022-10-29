26b65ab2fe8a28776b9759f9ba7e94acce0be697
Lucas Matos <lucaslmatos18@gmail.com>
2022-10-29 20:05:41 -0300

    C21 - Adequando comentários - commit final

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,28 +1,4 @@
 // Desafio 11 (Crie uma função de número de telefone)
-// (phoneNumber[0] === Math.sqrt(phoneNumber[1] * phoneNumber[2]))
-//     || (phoneNumber[1] === Math.sqrt(phoneNumber[2] * phoneNumber[3]))
-//     || (phoneNumber[2] === Math.sqrt(phoneNumber[3] * phoneNumber[4]))
-//     || (phoneNumber[3] === Math.sqrt(phoneNumber[4] * phoneNumber[5]))
-//     || (phoneNumber[4] === Math.sqrt(phoneNumber[5] * phoneNumber[6]))
-//     || (phoneNumber[5] === Math.sqrt(phoneNumber[6] * phoneNumber[7]))
-//     || (phoneNumber[6] === Math.sqrt(phoneNumber[7] * phoneNumber[8]))
-//     || (phoneNumber[7] === Math.sqrt(phoneNumber[8] * phoneNumber[9]))
-//     || (phoneNumber[8] === Math.sqrt(phoneNumber[9] * phoneNumber[10]))
-// Salvando código mais próximo do correto para testes:
-// if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
-//   for (let index = 0; index < phoneNumber.length; index += 1) {
-//     let cont = 0;
-//     for (let index2 = 0; index2 < phoneNumber.length; index2 += 1) {
-//       if (phoneNumber[index] === phoneNumber[index2]) {
-//         cont += 1;
-//       }
-//     } if (phoneNumber[index] < 0 || phoneNumber[index] > 9 || cont > 2) {
-//       return 'não é possível gerar um número de telefone com esses valores';
-//     }
-//   }
-//   let number = phoneNumber.join('');
-//   return `(${number.substring(0, 2)}) ${number.substring(2, 7)}-${number.substring(7, 11)}`;
-// Fim do Código
 function generatePhoneNumber(phoneNumber) {
   if (phoneNumber.length !== 11) return 'Array com tamanho incorreto.';
   let numO = phoneNumber.join('');
